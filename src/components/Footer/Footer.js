b1f6240987dfc92e0d0b9f685d8abbabc51754cc
interglobalmedia <interglobalmedia@gmail.com>
2019-04-26 08:04:46 -0400

    INT-15 #resolve #time 25m #comment add contact sitemap rss links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faRss,
+  faDirections,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons"
 
 const Footer = () => {
   return (
@@ -10,6 +17,23 @@ const Footer = () => {
         color: "#717171",
       }}
     >
+      <div style={{ marginBottom: "1rem" }}>
+        <a
+          href="mailto:interglobalmedia@gmail.com"
+          style={{ boxShadow: "none", marginRight: "1rem" }}
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+          Contact
+        </a>
+        <Link to="/rss.xml" style={{ boxShadow: "none", marginRight: "1rem" }}>
+          <FontAwesomeIcon icon={faDirections} />
+          Sitemap
+        </Link>
+        <Link to="/rss.xml" style={{ boxShadow: "none" }}>
+          <FontAwesomeIcon icon={faRss} />
+          RSS
+        </Link>
+      </div>
       © {new Date().getFullYear()}
       {` `}
       <a
