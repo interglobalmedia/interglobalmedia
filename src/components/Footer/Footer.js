379ebba255be3e4131a5662c578bd4c407de8767
interglobalmedia <interglobalmedia@gmail.com>
2019-04-25 21:25:30 -0400

    INT-11 #in-progress #comment chnage credits

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,21 @@ import React from "react"
 const Footer = () => {
   return (
     <footer
-      style={{ minHeight: "90px", paddingTop: "50px", textAlign: "center" }}
+      style={{
+        minHeight: "90px",
+        paddingTop: "50px",
+        textAlign: "center",
+        color: "#717171",
+      }}
     >
-      © {new Date().getFullYear()}, Built with
+      © {new Date().getFullYear()}
       {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <a
+        href="https://www.interglobalmedianetwork.com"
+        style={{ boxShadow: "none" }}
+      >
+        Inter-Global Media Network, Inc.
+      </a>
     </footer>
   )
 }
