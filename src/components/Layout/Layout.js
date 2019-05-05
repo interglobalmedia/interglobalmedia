9f1da1541da16d3190a5b1d5474eda46eb48c4ba
interglobalmedia <interglobalmedia@gmail.com>
2019-05-05 14:33:39 -0400

    INT-21 #in-progress #comment add Search Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {StaticQuery, graphql} from 'gatsby'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import Search from '../Search/Search'
 import './Layout.scss'
 
 const Layout = ({children}) => (
@@ -16,11 +17,10 @@ const Layout = ({children}) => (
             }
         `}
         render={data => (
-            <div style={{background: '#ffec8f'}}>
-                <header>
-                    <Header siteTitle={data.site.siteMetadata.title} />
-                </header>
+            <div style={{background: 'rgb(255,227,159)'}}>
+                <Header siteTitle={data.site.siteMetadata.title} />
                 <div className="Site">
+                    <Search />
                     <main className="Site-content">{children}</main>
                     <Footer />
                 </div>
