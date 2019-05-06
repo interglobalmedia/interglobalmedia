29886e314d0560ffb20d89df9ca6c0bdfea3b558
interglobalmedia <interglobalmedia@gmail.com>
2019-05-06 15:36:25 -0400

    INT-23 #resolve #time 8h 30m #comment add icons Link graphql

--- src/templates/blog-post-template.js
+++ src/templates/blog-post-template.js
@@ -27,7 +27,7 @@ const BlogPostTemplate = props => {
     } = props.data.markdownRemark.frontmatter
     const {prev, next} = props.pageContext
     return (
-        <Layout>
+        <>
             <SEO
                 title={title}
                 description={description}
@@ -35,101 +35,103 @@ const BlogPostTemplate = props => {
                 post
                 author={author}
             />
-            <div
-                style={{
-                    margin: '0 auto',
-                    color: 'rgb(47,0,0)',
-                }}
-            >
-                <div>
-                    {image && (
-                        <Img
-                            fluid={image.childImageSharp.fluid}
-                            style={{
-                                width: '100%',
-                                height: '400px',
-                                marginTop: '0',
-                            }}
-                        />
-                    )}
-                </div>
+            <Layout>
                 <div
                     style={{
-                        width: '100%',
-                        maxWidth: '1026px',
-                        margin: '3rem auto',
-                        textAlign: 'left',
+                        margin: '0 auto',
+                        color: 'rgb(47,0,0)',
                     }}
                 >
-                    <h1 style={{textAlign: 'center'}}>{title}</h1>
-                    <p style={{textAlign: 'center'}}>{date}</p>
-
-                    <div
-                        dangerouslySetInnerHTML={{
-                            __html: props.data.markdownRemark.html,
-                        }}
-                        style={{
-                            width: '90%',
-                            maxWidth: '1026px',
-                            margin: '0 auto',
-                        }}
-                    />
-
+                    <div>
+                        {image && (
+                            <Img
+                                fluid={image.childImageSharp.fluid}
+                                style={{
+                                    width: '100%',
+                                    height: '400px',
+                                    marginTop: '0',
+                                }}
+                            />
+                        )}
+                    </div>
                     <div
                         style={{
-                            width: '90%',
-                            margin: '0 auto',
+                            width: '100%',
                             maxWidth: '1026px',
-                            display: 'flex',
-                            flexDirection: 'column',
+                            margin: '3rem auto',
+                            textAlign: 'left',
                         }}
                     >
-                        <div style={{marginBottom: '1rem'}}>
-                            <span>Tagged in: </span>
-                            {tags.map((tag, i) => (
-                                <Link
-                                    to={`/tags/${tag}`}
-                                    key={i}
-                                    style={{boxShadow: 'none'}}
-                                >
-                                    <FontAwesomeIcon
-                                        icon={faTag}
-                                        style={{color: '#268bd2'}}
-                                    />{' '}
-                                    {tag}{' '}
-                                </Link>
-                            ))}
-                        </div>
-                        <div style={{marginBottom: '2rem'}}>
-                            <span>Categorized under: </span>
-                            {categories.map((category, i) => (
-                                <Link
-                                    to={`/categories/${category}`}
-                                    key={i}
-                                    style={{boxShadow: 'none'}}
-                                >
-                                    <FontAwesomeIcon
-                                        icon={faFolder}
-                                        style={{
-                                            color: '#268bd2',
-                                            marginRight: '0.25rem',
-                                        }}
-                                    />
-                                    {category}
-                                </Link>
-                            ))}
-                        </div>
-                        <Bio />
-                    </div>
-                    <div className="prev-next-div">
-                        <PrevNext
-                            prev={prev && prev.node}
-                            next={next && next.node}
+                        <h1 style={{textAlign: 'center'}}>{title}</h1>
+                        <p style={{textAlign: 'center'}}>{date}</p>
+
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: props.data.markdownRemark.html,
+                            }}
+                            style={{
+                                width: '90%',
+                                maxWidth: '1026px',
+                                margin: '0 auto',
+                            }}
                         />
+
+                        <div
+                            style={{
+                                width: '90%',
+                                margin: '0 auto',
+                                maxWidth: '1026px',
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}
+                        >
+                            <div style={{marginBottom: '1rem'}}>
+                                <span>Tagged in: </span>
+                                {tags.map((tag, i) => (
+                                    <Link
+                                        to={`/tags/${tag}`}
+                                        key={i}
+                                        style={{boxShadow: 'none'}}
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faTag}
+                                            style={{color: '#268bd2'}}
+                                        />{' '}
+                                        {tag}{' '}
+                                    </Link>
+                                ))}
+                            </div>
+                            <div style={{marginBottom: '2rem'}}>
+                                <span>Categorized under: </span>
+                                {categories.map((category, i) => (
+                                    <Link
+                                        to={`/categories/${category}`}
+                                        key={i}
+                                        style={{boxShadow: 'none'}}
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faFolder}
+                                            style={{
+                                                color: '#268bd2',
+                                                marginRight: '0.25rem',
+                                            }}
+                                        />
+                                        {category}
+                                    </Link>
+                                ))}
+                            </div>
+                            <Bio />
+                        </div>
+                        <div className="prev-next-div">
+                            <PrevNext
+                                prev={prev && prev.node}
+                                next={next && next.node}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Layout>
+            </Layout>
+        </>
     )
 }
 
