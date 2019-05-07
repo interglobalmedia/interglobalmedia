17234dac22740f48b5d33746c3836da2b5e6840d
interglobalmedia <interglobalmedia@gmail.com>
2019-05-07 07:46:23 -0400

    INT-25 #in-progress #comment fix typo

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -4,24 +4,24 @@ import Layout from '../components/Layout/Layout'
 import styled from '@emotion/styled'
 
 const TagsDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 3rem auto;
-  & h1 {
-    color: rgb(47,0,0);
-  }
-  & ul {
-    margin-left: -8rem;
-  }
-  & li {
-    list-style-type: square;
-    color: rgb(47,0,0);
-  }
-  & span {
-    color: color: rgba(88, 86, 86, 0.7);
-  }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 3rem auto;
+    & h1 {
+        color: rgb(47, 0, 0);
+    }
+    & ul {
+        margin-left: -8rem;
+    }
+    & li {
+        list-style-type: square;
+        color: rgb(47, 0, 0);
+    }
+    & span {
+        color: rgba(88, 86, 86, 0.7);
+    }
 `
 
 const Tags = props => {
