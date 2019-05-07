853b6cf5d73dfb6f4a883388b28168260dce89c7
interglobalmedia <interglobalmedia@gmail.com>
2019-05-07 17:39:48 -0400

    INT-27 #in-progress #comment bring back description to metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
-        title: `Inter 🌐 Media`,
-        titleTemplate: `Inter-Global Media Network, Inc. is where we design, develop, and teach experiences that make people's lives simpler.`,
+        siteTitle: `Inter 🌐 Media`,
+        siteTitleTemplate: `Inter-Global Media Network, Inc. is where we design, develop, and teach experiences that make people's lives simpler.`,
         description: `The Business Website for Inter-Global Media Network, Inc.`,
         siteUrl: `https://www.interglobalmedianetwork.com/`,
         image: '/images/profileSmall.png',
