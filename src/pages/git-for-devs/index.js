08c8ca0d527610f0c1e551075ec3315c5044443f
interglobalmedia <interglobalmedia@gmail.com>
2019-05-19 12:29:44 -0400

    INT-49 #in-progress #comment complete gfd page

--- src/pages/git-for-devs/index.js
+++ src/pages/git-for-devs/index.js
@@ -3,8 +3,57 @@ import styled from '@emotion/styled'
 import {Link} from 'gatsby'
 import Layout from '../../components/Layout/Layout'
 
+import gfd from '../../data/gfd'
+import {
+    WrapperDiv,
+    BackDiv,
+    H1Style,
+    H2Style,
+    IconSpan,
+    ApproachDiv,
+    ApproachSpan,
+    IncludesSpan,
+} from '../fsjs'
+
+const GitForDevsDiv = styled.div`
+    & img {
+        width: 100%;
+    }
+`
+export const GFDTagsDiv = styled.div`
+    color: rgb(216, 132, 46);
+    margin-bottom: 3rem;
+`
+
 const GitForDevs = () => {
-    return <div>This is the Git For Developers section.</div>
+    return (
+        <Layout>
+            <WrapperDiv>
+                <BackDiv>
+                    <Link to="/services">&larr; back</Link>
+                </BackDiv>
+                <H1Style>
+                    <IconSpan>{gfd.icon}</IconSpan> {gfd.title}
+                    <br />
+                </H1Style>
+                <GitForDevsDiv>
+                    <img src={gfd.image} alt={gfd.title} />
+                    <H2Style>{gfd.shortDescription}</H2Style>
+                    <ApproachDiv>
+                        <ApproachSpan>background:</ApproachSpan>{' '}
+                        {gfd.background}
+                    </ApproachDiv>
+                    <ApproachDiv>
+                        <ApproachSpan>approach:</ApproachSpan> {gfd.approach}
+                    </ApproachDiv>
+                    <GFDTagsDiv>
+                        <IncludesSpan>includes:</IncludesSpan> {gfd.tags} and
+                        more
+                    </GFDTagsDiv>
+                </GitForDevsDiv>
+            </WrapperDiv>
+        </Layout>
+    )
 }
 
 export default GitForDevs
