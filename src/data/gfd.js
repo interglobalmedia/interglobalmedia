c21dd119b0fdad5b6bd3c954ba031e72bfc9ebd0
interglobalmedia <interglobalmedia@gmail.com>
2019-05-19 12:31:39 -0400

    INT-49 #in-progress #comment add gfd data file

---
+++ src/data/gfd.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faGitSquare} from '@fortawesome/free-brands-svg-icons'
+
+import gitDevs from '../images/mimi-thian-737711-unsplash.jpg'
+
+const gfd = {
+    title: 'Git for Developers',
+    image: gitDevs,
+    shortDescription: 'Git Distributed Version Control For Developers',
+    background:
+        "When I decided to switch careers and become a developer, I found that most teaching, tutorials, courses, etc., only made cursory introductions to Git, and then went on to focus on code creation only. Git seemed to be an afterthought to them, even though it is an expected skill on the job. I found that to be a flawed approach. I taught myself Git along with learning how to code so that I could apply best practices from the get go. At first it was really hard, because I was learning on my own, and there wasn't too much beyond the basics to glean from. Things haven't changed too much since then. Developers keep on mostly rehashing the basics, but if one persists in getting to know Git, it becomes easier to find the right resources. That is what happened with me. The other thing that is very important in the acquisition of Git skills is working with others. Using Git by oneself is very different than using it in a team environment. Inter-Global Media Network, Inc. provides the opportunity to those that don't have access to teams to learn and use Git with other(s) in a structured environment.",
+    approach:
+        "I believe in working collaboratively and closely with my clients. Inter-Global Media Network inc.'s mission is to design, develop, and teach experiences that make people's lives simpler. Not only do my services entail building applications for clients but also providing them with a better understanding of the tools they depend on or want to learn.",
+    tags: [
+        'using the command line with git',
+        ', ',
+        'initializing the local repository',
+        ', ',
+        'adding file versions to the staging area',
+        ', ',
+        'committing file versions to the local repository',
+        ', ',
+        'pushing file versions to the remote repository',
+        ', ',
+        'creating the development branch for feature branch merging and local development',
+        ', ',
+        'creating the staging branch for the remote staging environment',
+        ', ',
+        'creating feature branches',
+        ', ',
+        'git stashing',
+        ', ',
+        'resolving merge conflicts',
+        ', ',
+        'creating the gh-pages branch on the remote repository for free static site hosting on github',
+    ],
+    icon: <FontAwesomeIcon icon={faGitSquare} />,
+}
+
+export default gfd
