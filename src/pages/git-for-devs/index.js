5f6d5b7cd219bbd11350f29cb6a975f080ee6d42
interglobalmedia <interglobalmedia@gmail.com>
2019-05-19 11:21:44 -0400

    INT-49 #in-progress #comment create empty  git for devs page

---
+++ src/pages/git-for-devs/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import {Link} from 'gatsby'
+import Layout from '../../components/Layout/Layout'
+
+const GitForDevs = () => {
+    return <div>This is the Git For Developers section.</div>
+}
+
+export default GitForDevs
