d8b5cf5f7f0dc65d8e1c68d11dcefcaa2c022f22
interglobalmedia <interglobalmedia@gmail.com>
2019-05-31 20:55:31 -0400

    INT-103 #in-progress #comment add posts data

---
+++ src/data/posts/featured-post-2.js
@@ -0,0 +1,28 @@
+import propagationImg from '../../images/mael-balland-1157034-unsplash.jpg'
+
+const blogSlug = '/blog'
+const id2Slug = '/why-your-computer-may-not-point-to-newly-propagated-site/'
+
+const featuredPost2Data = {
+    id: 2,
+    tagNames: [
+        'dns',
+        ', ',
+        'local hosts file',
+        ', ',
+        'ip address',
+        ', ',
+        'site propagation',
+        ', ',
+        'dns information groping',
+    ],
+    catNames: ['web hosting', ', ', 'changing web hosts'],
+    path: `${blogSlug}${id2Slug}`,
+    title:
+        'Why Your Local Machine (Computer) May Not be Pointing to Your Newly Propagated Site',
+    src: propagationImg,
+    excerpt:
+        'The other day I moved my business site, interglobalmedianetwork.com, from Github gh-pages to Netlify, where this site resides as well',
+}
+
+export default featuredPost2Data
