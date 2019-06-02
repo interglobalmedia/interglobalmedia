837fd894ab10a42eefccc719e19d16ab52871143
interglobalmedia <interglobalmedia@gmail.com>
2019-06-02 14:39:52 -0400

    INT-108 #in-progress #comment add wf-live-sites data

---
+++ src/data/wf-live-sites/wf-live-site-1.js
@@ -0,0 +1,39 @@
+import reactFlowUpdated2018 from '../../images/react-workflows-updated-2018.jpg'
+
+const wfLiveSite1Data = {
+    id: 1,
+    tagNames: [
+        'react js',
+        ', ',
+        'babel 7',
+        ', ',
+        'eslint',
+        ', ',
+        'webpack 4+',
+        ', ',
+        'jest',
+        ', ',
+        'es6 javascript',
+        ', ',
+        'dynamic imports',
+        ', ',
+        'css modules',
+        ', ',
+        'scss',
+        ', ',
+        'mini css extract plugin',
+        ', ',
+        'webpack dev server',
+        ', ',
+        'webpack dev middleware',
+        'gh pages',
+    ],
+    liveSiteUrl:
+        'https://interglobalmedia.github.io/react-workflow-updated-2018/#/',
+    siteRepoUrl:
+        'https://github.com/interglobalmedia/react-workflow-updated-2018',
+    src: reactFlowUpdated2018,
+    projectName: 'workflow 2018',
+}
+
+export default wfLiveSite1Data
---
+++ src/data/wf-live-sites/wf-live-site-4.js
@@ -0,0 +1,26 @@
+import canvasArt from '../../images/client-canvas-art.jpg'
+
+const wfLiveSite4Data = {
+    id: 4,
+    tagNames: [
+        'code refactoring',
+        ', ',
+        'workflow refactoring',
+        ', ',
+        'canvas 2d',
+        ', ',
+        'canvas animation',
+        ', ',
+        'babel 7',
+        ', ',
+        'webpack 4+',
+        ', ',
+        'prettier',
+    ],
+    liveSiteUrl: 'https://interglobalmedia.github.io/client-canvas-art/',
+    siteRepoUrl: 'https://github.com/interglobalmedia/client-canvas-art',
+    src: canvasArt,
+    projectName: 'client canvas art',
+}
+
+export default wfLiveSite4Data
