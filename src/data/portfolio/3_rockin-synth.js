501787c641861c2975b306c300b082eca3d1a56b
interglobalmedia <interglobalmedia@gmail.com>
2019-06-05 18:23:46 -0400

    INT-112 #resolve #time 2d 2h 24m #comment create indiv data files

---
+++ src/data/portfolio/3_rockin-synth.js
@@ -0,0 +1,27 @@
+import rockinSynth from '../../images/projects/rockin-synth.jpg'
+
+const rockinSynthData = {
+    id: 3,
+    title: 'Rockin Synth',
+    repository: `https://github.com/interglobalmedia/rockin-synth`,
+    website: `https://interglobalmedia.github.io/rockin-synth/`,
+    shortDescription: `Long description`,
+    image: rockinSynth,
+    shortDescription: `When the user presses a key, it creates a musical note corresponding to that of a real piano.
+    
+    When the user presses a key, a transition is triggered. When the user presses down on the key, it becomes a bit bigger and turns yellow. When the user lifts their finger from the key, the key scales down to its regular size and the yellow background disappears.`,
+    tags: [
+        'event listeners',
+        ', ',
+        'e.keyCode',
+        ', ',
+        'e.which',
+        ', ',
+        'kbd tags',
+        ', ',
+        'transitions',
+    ],
+    more: `https://interglobalmedia.github.io/portfolio/#/work/rockin-synth`,
+}
+
+export default rockinSynthData
---
+++ src/data/portfolio/6_node-chat-app.js
@@ -0,0 +1,25 @@
+import chatterbox from '../../images/projects/chatterbox.jpg'
+
+const chattrboxData = {
+    id: 6,
+    title: `Node Chat App`,
+    respository: `https://github.com/interglobalmedia/node-chat-app`,
+    website: `https://pacific-savannah-76659.herokuapp.com/`,
+    image: chatterbox,
+    shortDescription: `A real time communications app built with websockets/socket.io websocket library, Node.js, and Express. What is it for? For friends, family, and/or colleagues to connect in real time and communicate with each other. messages are timestamped. chattrbox also uses the geolocation API and Google Maps API Maps Url.`,
+    tags: [
+        'websockets',
+        ', ',
+        'socket.io',
+        ', ',
+        'nodejs',
+        ', ',
+        'express',
+        ', ',
+        'geolocation api',
+        ', ',
+        'google maps api maps url',
+    ],
+}
+
+export default chattrboxData
