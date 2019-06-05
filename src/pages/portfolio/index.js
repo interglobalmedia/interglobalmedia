45f744268f44af0208b47cd55e77e974d04ff890
interglobalmedia <interglobalmedia@gmail.com>
2019-06-05 18:20:33 -0400

    INT-112 #in-progress #comment remove portfolioData add indiv sections

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import {Link, graphql} from 'gatsby'
-import Image from 'gatsby-image'
 import Layout from '../../components/Layout/Layout'
-import portfolioData from '../../data/portfolio'
 import styled from '@emotion/styled'
 import Img from 'gatsby-image'
 import SEO from '../../components/Seo/Seo'
+import speechToTextData from '../../data/portfolio/1_speech-to-text'
+import theGrooveData from '../../data/portfolio/2_welcome-to-the-groove'
+import rockinSynthData from '../../data/portfolio/3_rockin-synth'
+import catWhispererData from '../../data/portfolio/4_cat-whisperer-app'
+import trumpTalksData from '../../data/portfolio/5_trump-talks-trash'
+import chattrboxData from '../../data/portfolio/6_node-chat-app'
+import nodeDrawingData from '../../data/portfolio/7_node-drawing-app'
 
 export const ColumnsDiv = styled.div`
     width: 90%;
@@ -37,7 +42,7 @@ export const ProjectUl = styled.div`
     }
 `
 
-const ProjectTitleLi = styled.li`
+export const ProjectTitleLi = styled.li`
     font-size: 1.1rem;
 `
 
@@ -53,7 +58,6 @@ export const ParaStyle = styled.p`
     align-items: flex-end;
     flex-direction: column;
     justify-content: flex-start;
-    margin-top: -1rem;
     color: rgb(88, 86, 86);
 `
 
@@ -66,43 +70,229 @@ const MoreP = styled.p`
 `
 
 const PortfolioIndex = props => {
-    const {data} = props
     const title = props.data.site.siteMetadata.title
     const keywords = props.data.site.siteMetadata.keywords
     return (
         <Layout>
             <SEO location={props.location} title={title} keywords={keywords} />
             <ColumnsDiv>
-                {portfolioData.map(project => (
-                    <ProjectUl key={project.id}>
-                        <ProjectTitleLi>
-                            <Link
-                                to={'/portfolio/portfolio-view'}
-                                state={project}
-                                key={project.title}
-                            >
-                                {project.title}
-                            </Link>
-                        </ProjectTitleLi>
-                        <img
-                            src={project.image}
-                            alt={project.title}
-                            key={project.id}
-                        />
-                        <ParaStyle>{project.shortDescription}</ParaStyle>
-                        <SpanStyle>{project.tags}</SpanStyle>
-                        <br />
-                        <li>
-                            <Link
-                                to={'/portfolio/portfolio-view'}
-                                state={project}
-                                key={project.title}
-                            >
-                                <MoreP>view more</MoreP>
-                            </Link>
-                        </li>
-                    </ProjectUl>
-                ))}
+                <ProjectUl key={speechToTextData.id}>
+                    <ProjectTitleLi>
+                        <Link
+                            to={'/portfolio/portfolio-view-1'}
+                            state={speechToTextData}
+                            key={speechToTextData.id}
+                        >
+                            {speechToTextData.title}
+                        </Link>
+                    </ProjectTitleLi>
+                    <Img
+                        fluid={
+                            props.data.portfolioImageOne.childImageSharp.fluid
+                        }
+                        alt={speechToTextData.title}
+                        key={speechToTextData.id}
+                    />
+                    <br />
+                    <ParaStyle>{speechToTextData.shortDescription}</ParaStyle>
+                    <SpanStyle>{speechToTextData.tags}</SpanStyle>
+                    <br />
+                    <li>
+                        <Link
+                            to={'/portfolio/portfolio-view-1'}
+                            state={speechToTextData}
+                            key={speechToTextData.title}
+                        >
+                            <MoreP>view more</MoreP>
+                        </Link>
+                    </li>
+                </ProjectUl>
+                <ProjectUl>
+                    <ProjectTitleLi>
+                        <Link
+                            to={'/portfolio/portfolio-view-2'}
+                            state={theGrooveData}
+                            key={theGrooveData.id}
+                        >
+                            {theGrooveData.title}
+                        </Link>
+                    </ProjectTitleLi>
+                    <Img
+                        fluid={
+                            props.data.portfolioImageTwo.childImageSharp.fluid
+                        }
+                        alt={theGrooveData.title}
+                        key={theGrooveData.id}
+                    />
+                    <br />
+                    <ParaStyle>{theGrooveData.shortDescription}</ParaStyle>
+                    <SpanStyle>{theGrooveData.tags}</SpanStyle>
+                    <br />
+                    <li>
+                        <Link
+                            to={'/portfolio/portfolio-view-2'}
+                            state={theGrooveData}
+                            key={theGrooveData.title}
+                        >
+                            <MoreP>view more</MoreP>
+                        </Link>
+                    </li>
+                </ProjectUl>
+                <ProjectUl>
+                    <ProjectTitleLi>
+                        <Link
+                            to={'/portfolio/portfolio-view-3'}
+                            state={rockinSynthData}
+                            key={theGrooveData.id}
+                        >
+                            {rockinSynthData.title}
+                        </Link>
+                    </ProjectTitleLi>
+                    <Img
+                        fluid={
+                            props.data.portfolioImageThree.childImageSharp.fluid
+                        }
+                        alt={rockinSynthData.title}
+                        key={rockinSynthData.id}
+                    />
+                    <br />
+                    <ParaStyle>{rockinSynthData.shortDescription}</ParaStyle>
+                    <SpanStyle>{rockinSynthData.tags}</SpanStyle>
+                    <br />
+                    <li>
+                        <Link
+                            to={'/portfolio/portfolio-view-3'}
+                            state={rockinSynthData}
+                            key={rockinSynthData.title}
+                        >
+                            <MoreP>view more</MoreP>
+                        </Link>
+                    </li>
+                </ProjectUl>
+                <ProjectUl>
+                    <ProjectTitleLi>
+                        <Link
+                            to={'/portfolio/portfolio-view-4'}
+                            state={catWhispererData}
+                            key={catWhispererData.id}
+                        >
+                            {catWhispererData.title}
+                        </Link>
+                    </ProjectTitleLi>
+                    <Img
+                        fluid={
+                            props.data.portfolioImageFour.childImageSharp.fluid
+                        }
+                        alt={catWhispererData.title}
+                        key={catWhispererData.id}
+                    />
+                    <br />
+                    <ParaStyle>{catWhispererData.shortDescription}</ParaStyle>
+                    <SpanStyle>{catWhispererData.tags}</SpanStyle>
+                    <br />
+                    <li>
+                        <Link
+                            to={'/portfolio/portfolio-view-4'}
+                            state={catWhispererData}
+                            key={catWhispererData.title}
+                        >
+                            <MoreP>view more</MoreP>
+                        </Link>
+                    </li>
+                </ProjectUl>
+                <ProjectUl>
+                    <ProjectTitleLi>
+                        <Link
+                            to={'/portfolio/portfolio-view-5'}
+                            state={trumpTalksData}
+                            key={trumpTalksData.id}
+                        >
+                            {trumpTalksData.title}
+                        </Link>
+                    </ProjectTitleLi>
+                    <Img
+                        fluid={
+                            props.data.portfolioImageFive.childImageSharp.fluid
+                        }
+                        alt={trumpTalksData.title}
+                        key={trumpTalksData.id}
+                    />
+                    <br />
+                    <ParaStyle>{trumpTalksData.shortDescription}</ParaStyle>
+                    <SpanStyle>{trumpTalksData.tags}</SpanStyle>
+                    <br />
+                    <li>
+                        <Link
+                            to={'/portfolio/portfolio-view-5'}
+                            state={trumpTalksData}
+                            key={trumpTalksData.title}
+                        >
+                            <MoreP>view more</MoreP>
+                        </Link>
+                    </li>
+                </ProjectUl>
+                <ProjectUl>
+                    <ProjectTitleLi>
+                        <Link
+                            to={'/portfolio/portfolio-view-6'}
+                            state={chattrboxData}
+                            key={chattrboxData.id}
+                        >
+                            {chattrboxData.title}
+                        </Link>
+                    </ProjectTitleLi>
+                    <Img
+                        fluid={
+                            props.data.portfolioImageSix.childImageSharp.fluid
+                        }
+                        alt={chattrboxData.title}
+                        key={chattrboxData.id}
+                    />
+                    <br />
+                    <ParaStyle>{chattrboxData.shortDescription}</ParaStyle>
+                    <SpanStyle>{chattrboxData.tags}</SpanStyle>
+                    <br />
+                    <li>
+                        <Link
+                            to={'/portfolio/portfolio-view-6'}
+                            state={chattrboxData}
+                            key={chattrboxData.title}
+                        >
+                            <MoreP>view more</MoreP>
+                        </Link>
+                    </li>
+                </ProjectUl>
+                <ProjectUl>
+                    <ProjectTitleLi>
+                        <Link
+                            to={'/portfolio/portfolio-view-7'}
+                            state={nodeDrawingData}
+                            key={nodeDrawingData.id}
+                        >
+                            {nodeDrawingData.title}
+                        </Link>
+                    </ProjectTitleLi>
+                    <Img
+                        fluid={
+                            props.data.portfolioImageSeven.childImageSharp.fluid
+                        }
+                        alt={nodeDrawingData.title}
+                        key={nodeDrawingData.id}
+                    />
+                    <br />
+                    <ParaStyle>{nodeDrawingData.shortDescription}</ParaStyle>
+                    <SpanStyle>{nodeDrawingData.tags}</SpanStyle>
+                    <br />
+                    <li>
+                        <Link
+                            to={'/portfolio/portfolio-view-7'}
+                            state={nodeDrawingData}
+                            key={nodeDrawingData.title}
+                        >
+                            <MoreP>view more</MoreP>
+                        </Link>
+                    </li>
+                </ProjectUl>
             </ColumnsDiv>
         </Layout>
     )
@@ -116,6 +306,67 @@ export const indexQuery = graphql`
             siteMetadata {
                 title
                 keywords
+            }
+        }
+        portfolioImageOne: file(
+            relativePath: {eq: "projects/speech-to-text.jpg"}
+        ) {
+            childImageSharp {
+                fluid(maxWidth: 1026) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        portfolioImageTwo: file(
+            relativePath: {eq: "projects/welcome-to-the-groove.jpg"}
+        ) {
+            childImageSharp {
+                fluid(maxWidth: 1026) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        portfolioImageThree: file(
+            relativePath: {eq: "projects/rockin-synth.jpg"}
+        ) {
+            childImageSharp {
+                fluid(maxWidth: 1026) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        portfolioImageFour: file(
+            relativePath: {eq: "projects/cat-whisperer-app.jpg"}
+        ) {
+            childImageSharp {
+                fluid(maxWidth: 1026) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        portfolioImageFive: file(
+            relativePath: {eq: "projects/trump-talks.jpg"}
+        ) {
+            childImageSharp {
+                fluid(maxWidth: 1026) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        portfolioImageSix: file(relativePath: {eq: "projects/chatterbox.jpg"}) {
+            childImageSharp {
+                fluid(maxWidth: 1026) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        portfolioImageSeven: file(
+            relativePath: {eq: "projects/node-collaborative-drawing-app.jpg"}
+        ) {
+            childImageSharp {
+                fluid(maxWidth: 1026) {
+                    ...GatsbyImageSharpFluid
+                }
             }
         }
     }
