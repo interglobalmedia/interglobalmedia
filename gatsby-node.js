d67d8d101712dcce5f17993fffdd00d49e2ecbd1
interglobalmedia <interglobalmedia@gmail.com>
2019-07-09 13:33:39 -0400

    INT-141 #in-progress #comment turn off source maps on build

--- gatsby-node.js
+++ gatsby-node.js
@@ -141,8 +141,22 @@ exports.onCreateNode = ({node, getNode, actions}) => {
         })
     }
 }
-// turn off sourcemaps in production build no longer works
+// // added for styled components 4+
+// exports.onCreateWebpackConfig = ({ stage, actions }) => {
+//     if (stage === 'develop') {
+//         actions.setWebpackConfig({
+//             devtool: 'cheap-module-source-map',
+//         })
+//     }
+// }
+
+//turn off sourcemaps in production build no longer works
 exports.onCreateWebpackConfig = ({actions, stage}) => {
+    // if (stage === 'develop') {
+    //     actions.setWebpackConfig({
+    //         devtool: 'cheap-module-source-map',
+    //     })
+    // }
     // If production JavaScript and CSS build
     if (stage === 'build-javascript') {
         // Turn off source maps
