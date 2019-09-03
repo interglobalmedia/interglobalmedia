3f0223c5e89b4329d5b9c907f17fd975d85c0f01
interglobalmedia <interglobalmedia@gmail.com>
2019-09-03 11:04:06 -0400

    Add latest podcast number 12 to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -119,12 +119,12 @@ const IndexPage = props => {
                             </a>
                         </H1Style>
                         <iframe
-                            src="https://anchor.fm/maria-campbell/embed/episodes/Multer--Amazon-S3-and-AWS-Amplify-e4qqmr"
+                            src="https://anchor.fm/maria-campbell/embed/episodes/Specialization-The-Importance-of-Not-Spreading-Oneself-Too-Thin-e576bk"
                             height="102px"
                             width="400px"
                             frameBorder="0"
                             scrolling="no"
-                            title="Multer, Amazon S3 and AWS Amplify"
+                            title="Specialization: The Importance of Not Spreading Oneself Too Thin"
                         ></iframe>
                     </PodDiv>
                     <H1Style as="h1">
