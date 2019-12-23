ad08ac567cf2f7edaaf56bf9b59fe36d05d2ca0c
interglobalmedia <interglobalmedia@gmail.com>
2019-12-23 05:39:04 -0500

    Add back manifest and offline and see what happens on remote

--- gatsby-config.js
+++ gatsby-config.js
@@ -301,6 +301,19 @@ module.exports = {
                 ],
             },
         },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Blog`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`,
+            },
+        },
+        `gatsby-plugin-offline`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-plugin-typography`,
