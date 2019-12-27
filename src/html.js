a0eac49c92af677698ed62e8146a4cf0f04f2103
interglobalmedia <interglobalmedia@gmail.com>
2019-12-27 16:15:57 -0500

    Add script to ssr

--- src/html.js
+++ src/html.js
@@ -13,7 +13,6 @@ export default function HTML(props) {
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
-                <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="fc478bdc-8e68-4068-b4c3-e810e0d7b378";(function(){d = document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
                 {props.headComponents}
             </head>
             <body {...props.bodyAttributes}>
