578d3e3b087931febcafcecc415246c6c0382df8
interglobalmedia <interglobalmedia@gmail.com>
2019-12-30 00:40:19 -0500

    Only use GATSBY_ variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,6 @@ if (env) {
     require('dotenv').config({path: `./.env.${prodEnv}`})
 }
 
-const CRISP_WEBSITE_ID = process.env.CRISP_WEBSITE_ID
-
 const GATSBY_CRISP_WEBSITE_ID = process.env.GATSBY_CRISP_WEBSITE_ID
 
 const netlifyCmsPaths = {
@@ -330,8 +328,7 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-crisp-chat',
             options: {
-                websiteId:
-                    `${CRISP_WEBSITE_ID}` || `${GATSBY_CRISP_WEBSITE_ID}`,
+                websiteId: `${GATSBY_CRISP_WEBSITE_ID}`,
                 enableDuringDevelop: true, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
                 defer: false, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
                 enableImprovedAccessibility: true, // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
