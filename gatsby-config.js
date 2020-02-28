5cf9f03aecb3b5a116633eebe15787418fbb5801
interglobalmedia <interglobalmedia@gmail.com>
2020-02-28 00:38:57 -0500

    dd video embed

--- gatsby-config.js
+++ gatsby-config.js
@@ -164,6 +164,21 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
+                    {
+                        resolve: 'gatsby-remark-embed-video',
+                        options: {
+                            ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+                            related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+                            noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+                            urlOverrides: [
+                                {
+                                    id: 'youtube',
+                                    embedURL: videoId =>
+                                        `https://www.youtube-nocookie.com/embed/${videoId}`,
+                                },
+                            ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+                        },
+                    },
                     netlifyCmsPaths, // Including in your Gatsby plugins will transform any paths in your frontmatter
                     {
                         resolve: `gatsby-remark-images`,
