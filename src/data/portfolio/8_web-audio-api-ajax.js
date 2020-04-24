37afc20d9282a5c4078440892b3f71858b30d1f1
interglobalmedia <interglobalmedia@gmail.com>
2020-04-24 11:08:10 -0400

    Add confetti data

---
+++ src/data/portfolio/8_web-audio-api-ajax.js
@@ -0,0 +1,36 @@
+import webAudioApiAjax from '../../images/projects/web-audio-api-ajax.jpg'
+
+const webAudioApiAjaxData = {
+    id: 8,
+    title: 'Web Audio API Meets a Sprinkle of Confetti',
+    repository: 'https://github.com/interglobalmedia/web-audio-api-ajax',
+    website: 'https://interglobalmedia.github.io/web-audio-api-ajax/',
+    image: webAudioApiAjax,
+    shortDescription: `A Web Audio API app which uses AJAX to retrieve a locally hosted audio file. When the user clicks on the start button, the background and text color of the button change, as does the background color of the page. Music starts playing, and confetti populates the page. When the user clicks on the stop button, the music stops, the background and text color of the button change, as well as the background color of the page. When the user clicks on the refresh button, the page and buttons are set to their default state.`,
+    tags: [
+        'web audio api',
+        ', ',
+        'ajax',
+        ', ',
+        'confetti-js',
+        ', ',
+        'npm',
+        ', ',
+        'canvas',
+        ', ',
+        'random colors',
+        ', ',
+        'gh-pages',
+        ', ',
+        'cdnjs',
+        ', ',
+        'buffer',
+        ', ',
+        'scripts',
+        ', ',
+        'node-sass',
+    ],
+    more: 'https://github.com/interglobalmedia/web-audio-api-ajax',
+}
+
+export default webAudioApiAjaxData
