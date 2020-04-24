f346821c7f151728beb08bae72f91e3e3edf005b
interglobalmedia <interglobalmedia@gmail.com>
2020-04-24 15:40:45 -0400

    Add new brand icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -324,7 +324,7 @@ module.exports = {
                 background_color: `#ffffff`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`,
+                icon: `src/images/interglobalmedia-brand.png`,
             },
         },
         `gatsby-plugin-offline`,
