7d207e84e27ec2571a9c49bedec00aefcfa8c35e
Maria Campbell <interglobalmedia@gmail.com>
2021-06-03 11:50:15 -0400

    Add portfolio view 10

---
+++ src/data/portfolio/10_custom_html5_video_player.js
@@ -0,0 +1,32 @@
+import customHTML5Video from '../../images/projects/custom-html5-video-player.jpg'
+
+const customHTML5VideoData = {
+    id: 10,
+    title: 'Custom HTML5 Video Player and Shadow DOM',
+    repository: 'https://github.com/interglobalmedia/custom-html5-video-player',
+    website: 'https://interglobalmedia.github.io/custom-html5-video-player/',
+    image: customHTML5Video,
+    shortDescription: 'An app which creates a custom HTML5 video player using the Shadow DOM so that the video player can go full screen without issue. This custom HTML5 video player looks more like a YouTube or Netflix experience insgtead of a user agent stylesheet!',
+    tags: [
+        'fullscreen-api',
+        ', ',
+        'html-5-video',
+        ', ',
+        'html-5',
+        ', ',
+        'cross-browser-issues',
+        ', ',
+        'cascading-stylesheets',
+        ', ',
+        'shadow-dom',
+        ', ',
+        'user-agent-stylesheets',
+        ', ',
+        'pseudo-elements',
+        ', ',
+        'javascript'
+    ],
+    more: 'https://www.interglobalmedianetwork.com/blog/2017-01-04-creating-a-custom-html5-video-player-and-the-shadow-dom/'
+}
+
+export default customHTML5VideoData
