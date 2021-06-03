8fadde927acfed52072c7746bb9128b689cf9372
Maria Campbell <interglobalmedia@gmail.com>
2021-06-03 09:10:37 -0400

    Add portfolio view 9

---
+++ src/data/portfolio/9_omdb_api_nodejs.js
@@ -0,0 +1,48 @@
+import omdbApiNodejs from '../../images/projects/omdb-api-nodejs-app.jpg'
+
+const omdbApiNodejsData = {
+    id: 9,
+    title: 'OMDB API with Node.js and Async/Await',
+    repository: 'https://github.com/interglobalmedia/omdb-api-nodejs',
+    website: 'https://omdb-api-nodejs.herokuapp.com/',
+    image: omdbApiNodejs,
+    shortDescription: 'An app which implements the OMDB API Using Nodejs, protects your API key, and deploys to Heroku. It uses the Got HTTP library, which replaces the deprecated Request library, and which supports Async/await (among other things!).',
+    tags: [
+        'async-await',
+        ', ',
+        'dotenv',
+        ', ',
+        'ejs',
+        ', ',
+        'ejs-lint',
+        ', ',
+        'eslint',
+        ', ',
+        'express',
+        ', ',
+        'got',
+        ', ',
+        'heroku',
+        ', ',
+        'local-scripts',
+        ', ',
+        'modern-javascript',
+        ', ',
+        'node.js',
+        ', ',
+        'node-modules',
+        ', ',
+        'nodemon',
+        ', ',
+        'node-sass',
+        ', ',
+        'omdb-api',
+        ', ',
+        'pagination',
+        ', ',
+        'scss'
+    ],
+    more: 'https://github.com/interglobalmedia/omdb-api-nodejs'
+}
+
+export default omdbApiNodejsData
