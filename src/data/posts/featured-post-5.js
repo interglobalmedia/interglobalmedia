81c9064e0c56fb63d1a725d31d4cb63956921c93
Maria Campbell <interglobalmedia@gmail.com>
2021-06-04 07:38:46 -0400

    Make title long enough

--- src/data/posts/featured-post-5.js
+++ src/data/posts/featured-post-5.js
@@ -21,7 +21,7 @@ const featuredPost5Data = {
         'linters'
     ],
     path: `${blogSlug}${id5Slug}`,
-    title: 'How to enable async await in a Node.js/Express application with ESLint',
+    title: 'How to enable async/await in Node.js with ESLint',
     src: googleMeet,
     excerpt: 'I was going through the final project for the course I am teaching at CityTech called Dynamic Web 1 which includes covering Modern Javascript and introduces Node.js, environment variables, .dotenv, ESLint rules, APIs, API keys, and how to protect them, the creation of local scripts in package.json, the EJS templating engine for Express.js, and async/await'
 }
